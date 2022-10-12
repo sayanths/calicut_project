5c5a1ee47431ba61406ce1197d9543b66cf6ad1f
sayanth A <sayanthsayu619@gmail.com>
2022-10-12 22:34:55 +0530

    custom widget created

---
+++ lib/app/utiles/widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+const SizedBox height10 = SizedBox(
+  height: 10,
+);
+
+const SizedBox height30 = SizedBox(
+  height: 30,
+);
+
+const SizedBox height50 = SizedBox(
+  height: 50,
+);
+const SizedBox height20 = SizedBox(
+  height: 20,
+);
