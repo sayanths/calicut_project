a872c467e3e234b36bdb5ffb90bb0bb847b6a0c4
sayanth A <sayanthsayu619@gmail.com>
2022-10-12 22:34:07 +0530

    custom color created

---
+++ lib/app/core/color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/animation.dart';
+import 'package:flutter/material.dart';
+
+const Color green = Color.fromARGB(255, 5, 170, 159);
+const Color white = Color.fromARGB(255, 255, 254, 254);
+const Color grey = Color.fromARGB(255, 164, 163, 163);
+const Color black = Color.fromARGB(255, 0, 0, 0);
+const Color red = Color.fromARGB(255, 242, 0, 0);
+const Color greenlight = Color.fromARGB(255, 0, 152, 8);
+const Color yellow = Color.fromARGB(255, 209, 198, 0);
+const Color darkGreen = Color.fromARGB(255, 3, 61, 5);
